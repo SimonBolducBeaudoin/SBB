3d27b8e8a0ba7d4175da21476f03b8bf3a6dfd18
Unknown <simonbolducbeaudoin@hotmail.fr>
2021-12-01 14:38:04 -0500

    Version number update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md","r") as fh:
 
 setuptools.setup(
     name="SBB",
-    version="0.0.18",
+    version="0.0.19",
     author="Simon Bolduc Beaudoin",
     author_email="Simon.Bolduc.Beaudoin@usherbrooke.ca",
     long_description=long_description,
