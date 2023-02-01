5fa99a81e64b94b13d5f2dddf298974cd927ce47
Unknown <simonbolducbeaudoin@hotmail.fr>
2023-02-01 16:35:16 -0500

    Add Matplotlib_extra

--- SBB/Matplotlib_extra/style.py
+++ SBB/Matplotlib_extra/style.py
@@ -5,5 +5,22 @@ color_list      = ['b', 'g', 'r', 'c','m','y','k']
 linestyle_list  = ['-','--','-.',':']
 marker_list     = ['*','+','x','o','.','D','s',',','v','^','<','>','1','2','3','4'] 
 
+
 def gen_cycler(**options):
     return cycler(color=color_list[:4],linestyle=linestyle_list[:4],marker=marker_list[:4])
+    
+if __name__ == "__main__" :
+    import matplotlib
+    ratio =1.62
+    width = 14*1.5
+    matplotlib.rcParams['figure.figsize'] = [width,width/ratio]
+
+    font = {'family' : 'None',
+            'weight' : 'None',
+            'size'   : 15}
+
+    matplotlib.rc('font', size=20)
+
+    rcParams['lines.linewidth'] = 3
+    rcParams['lines.markersize'] = 15
+    rcParams['lines.markeredgewidth'] = 2.5
