eee8d2f44ec7be5c07585c5aa5b2d34838892412
SBB <simonbolducbeaudoin@gmail.com>
2023-02-02 09:01:31 -0500

    Added get_untreated_files to DataManager

--- SBB/Utilities/Data_Manager.py
+++ SBB/Utilities/Data_Manager.py
@@ -8,6 +8,49 @@ import numpy
 from SBB.Utilities.python_extra import super_enumerate
 from SBB.Utilities.numpy_extra import argwhere_tuple
 
+def get_untreated_files(folder,pattern= 'exp_??????-??????.npz',ReDo=False,AddIgnore=True,LookForLonelyIgnore=True):
+    """
+    Used to get a list of files that have not been treated yet (aka not flagged with ignore yet).
+        
+    Basic use (ReDo=False,AddIgnore=False,LookForLonelyIgnore=False)
+        Looks inside folder for files matching "pattern", 
+        also looks for files matching "pattern"+.ignore 
+        and return a list of pattern files excluding those that have a corresponding .ignore sister
+        Exemple : 
+        If folder content is :
+             'exp_000000-000000.npz'
+             'exp_000000-000001.npz'
+             'exp_000000-000001.npz.ignore'
+        then it returns
+             ['exp_000000-000000.npz']
+             
+    Options :
+        ReDo (False by default)
+            Doesn't filter for .ignore file and hence returns all files matching the pattern
+        AddIgnore (True by default)
+            Adds .ignore to files name (modifies files names)
+        LookForLonelyIgnore
+            Looks if some "pattern.ignore" files dont have a matching "parttern" file and add them to the list
+    """
+    pattern_list = glob.glob(folder+os.sep+pattern)
+    ignore_list  = glob.glob(folder+os.sep+pattern+'.ignore')
+    
+    # New_list with only experiments that have not already been processed    
+    not_done_list = [ exp for exp in pattern_list if (exp + '.ignore' not in ignore_list) ]
+
+    # Adds .ignore to the latter
+    if AddIgnore :
+        for exp in not_done_list :
+            os.rename(exp,exp+'.ignore')
+
+    # Get the "parttern.ignore" files that don't have a corresponding "pattern" file
+    lonely_ignore_list = [exp.replace('.ignore', '') for exp in ignore_list if ( (exp.replace('.ignore', '') not in pattern_list) and (LookForLonelyIgnore) ) ]
+    
+    if ReDo :
+        return pattern_list + lonely_ignore_list
+    else :
+        return not_done_list + lonely_ignore_list
+
 def remove_zeros_subarrays(arr):
     """
     Returns only non-zeros elements along the firts dimension/axis = 0
