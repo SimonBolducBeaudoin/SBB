a7eae619439544664155084e69252399e454510f
Unknown <simonbolducbeaudoin@hotmail.fr>
2023-04-14 13:18:23 -0400

    Phys fix imports

--- SBB/Phys/Phys_Stat.py
+++ SBB/Phys/Phys_Stat.py
@@ -1,5 +1,8 @@
 #!/bin/env/python
 #! -*- coding: utf-8 -*-
 
+import numpy as _np
+from scipy import constants  as C
+
 def BoseEinstein(f,T):
-    return 1.0/(numpy.exp(_h*f/(_kb*T))-1.0)
+    return 1.0/(_np.exp(C.h*f/(C.k*T))-1.0)
