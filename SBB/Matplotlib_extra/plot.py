6341b25444e9e413668fa419f89386be3bcedbb4
Unknown <simonbolducbeaudoin@hotmail.fr>
2023-06-01 11:46:37 -0400

    Matplotlib_extra added nanplot

--- SBB/Matplotlib_extra/plot.py
+++ SBB/Matplotlib_extra/plot.py
@@ -3,6 +3,10 @@
 
 import numpy as _np
 
+def nanplot(ax,x,y,**kwargs):
+    not_nan = ~(_np.isnan(y)) 
+    return ax.plot(x[not_nan],y[not_nan],**kwargs)
+
 def plot_interval(ax,x,Y,std=True,n_sigma=3,min_max=True,label=None,linestyle=None,marker=None,alpha=[None,0.75,0.5], **kwargs):
     """
     Almost the same as plot but plota Y.mean(axis=0) and (optionnaly) some visual metrics on deviation.
@@ -26,12 +30,19 @@ def plot_interval(ax,x,Y,std=True,n_sigma=3,min_max=True,label=None,linestyle=No
     line, same output as ax.plot
     """
     Y_m = _np.nanmean( Y,axis=0 )
-    line, = ax.plot(x, Y_m ,label=label,marker=marker,linestyle=linestyle,**kwargs)
-    if std :
-        Y_std = _np.nanstd( Y,axis=0 )
-        ax.fill_between(x, Y_m + n_sigma*Y_std, Y_m - n_sigma*Y_std, facecolor = line.get_color(),alpha=alpha[1],**kwargs)
-    if min_max :    
-        Y_max = _np.nanmax( Y,axis=0 )
-        Y_min = _np.nanmin( Y,axis=0 )
-        ax.fill_between(x, Y_max,Y_min, facecolor = line.get_color(),alpha=alpha[2],**kwargs)
+    not_nan = ~(_np.isnan(Y_m)) 
+    
+    all_nan = all( not_nan ==False)
+    
+    if all_nan :
+        line = None
+    else :
+        line, = ax.plot(x[not_nan], Y_m[not_nan] ,label=label,marker=marker,linestyle=linestyle,**kwargs)
+        if std :
+            Y_std = _np.nanstd( Y,axis=0 )
+            ax.fill_between(x[not_nan], (Y_m + n_sigma*Y_std)[not_nan], (Y_m - n_sigma*Y_std)[not_nan], facecolor = line.get_color(),alpha=alpha[1],**kwargs)
+        if min_max :    
+            Y_max = _np.nanmax( Y,axis=0 )
+            Y_min = _np.nanmin( Y,axis=0 )
+            ax.fill_between(x[not_nan], Y_max[not_nan],Y_min[not_nan], facecolor = line.get_color(),alpha=alpha[2],**kwargs)
     return line, 
