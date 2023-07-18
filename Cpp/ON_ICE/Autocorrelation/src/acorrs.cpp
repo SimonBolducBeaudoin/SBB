491a353bbe29602ceb1f79f7409a317ddd53dfef
Unknown <simonbolducbeaudoin@hotmail.fr>
2023-07-18 11:21:51 -0400

    Removing Cpp

--- Cpp/ON_ICE/Autocorrelation/src/acorrs.cpp
+++
@@ -1,7 +0,0 @@
-#include "acorrs.tpp"
-
-// Instantiating classes for the types we want to support
-template class ACorrUpTo<uint8_t>;
-template class ACorrUpTo<int8_t>;
-template class ACorrUpTo<uint16_t>;
-template class ACorrUpTo<int16_t>;
--- Cpp/ON_ICE/README.txt
+++
@@ -1,3 +0,0 @@
-ON ICE is a folder where I put code that 
-I might want to re-use/work-on eventually 
-but that doesn't have any value for now
