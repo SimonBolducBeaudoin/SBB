b9ae4beb45c8d551d91677273e5044ababc5c0ba
Simon Bolduc Beaudoin <Simon.Bolduc.Beaudoin@usherbrooke.ca>
2023-09-16 14:28:29 -0400

    rm bash files

--- make_Cpp_COPY_TO_TARGET.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp -u AFAP/aCorrs-OTF/bin/acorrs_wrapper.* SBB/AutoCorr/.
-cp -u AFAP/aCorrs-OTF/bin/acorrs_otf.py SBB/AutoCorr/.
-cp -u AFAP/Time_quadratures/bin/time_quadratures.* SBB/Time_quadratures/.
-cp -u AFAP/Histograms/bin/histograms.* SBB/Histograms/.
-cp -u AFAP/Moments_cumulants/bin/moments_cumulants.* SBB/Histograms/.
-cp -u AFAP/Omp_extra/bin/omp_extra.* SBB/Omp_extra/.
