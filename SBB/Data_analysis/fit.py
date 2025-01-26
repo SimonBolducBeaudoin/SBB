9664532f2189d337c7308e66abd3f56d2f2128f9
Simon Bolduc Beaudoin <simonbolducbeaudoin@gmail.com>
2025-01-26 09:44:09 -0500

    (Fix) Fixed 2D fit function

--- SBB/Data_analysis/fit.py
+++ SBB/Data_analysis/fit.py
@@ -166,7 +166,7 @@ def polyfit_multi_between(x,Y,Xth_low=None,Xth_high=None,deg=1):
 
 def lstsq_2D(x0,x1,y,para,fit_func,tol=1e-8,full_output=0): 
     def residuals(p):
-        return y-fit_func(x0,x1,p)
+        return (y-fit_func(x0,x1,p)).flatten()
     return _leastsq(residuals,para,full_output=full_output,ftol=tol,xtol=tol)
     
 def lstsq(x,y,para,fit_func,tol=1e-8,full_output=0): 
